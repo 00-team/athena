3d7be98b9ef237778b0b118e0cbb9fd55b076fb0
007 <dr007cc@gmail.com>
2023-03-08 19:52:53 +0330

    fix: add a error handler

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 
+import html
+import json
+import traceback
+
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.error import TelegramError
 from telegram.ext import Application, CallbackQueryHandler, ChatMemberHandler
 from telegram.ext import CommandHandler, ContextTypes, MessageHandler, filters
@@ -42,6 +47,36 @@ async def forward_to_all(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
         f'ok. forward your message: {FORWARD_ALL[user,id]}'
+    )
+
+
+async def error_handler(update: object, ctx: ContextTypes.DEFAULT_TYPE):
+    logger.error(
+        msg='Exception while handling an update:',
+        exc_info=ctx.error
+    )
+
+    tb_list = traceback.format_exception(
+        None, ctx.error, ctx.error.__traceback__
+    )
+    tb_string = ''.join(tb_list)
+
+    if isinstance(update, Update):
+        update_str = json.dumps(update.to_dict(), indent=2, ensure_ascii=False)
+    else:
+        update_str = str(update)
+
+    message = (
+        f'An exception was raised while handling an update\n\n'
+        f'<pre>{html.escape(update_str)}</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(ctx.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(ctx.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )
+
+    # Finally, send the message
+    await ctx.bot.send_message(
+        chat_id=SECRETS['DEVELOPER'], text=message, parse_mode=ParseMode.HTML
     )
 
 
@@ -144,6 +179,7 @@ def main():
     setup_databases()
 
     application = Application.builder().token(SECRETS['TOKEN']).build()
+    application.add_error_handler(error_handler)
 
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('help', help_command))
